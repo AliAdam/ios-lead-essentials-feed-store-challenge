d936d9f5ea6e81cc68e957196a42ece843557e0f
AliAdam <eng.aliadam@gmail.com>
2021-03-01 20:18:03 +0300

    add InMemoryFeedStore class file

---
+++ FeedStoreChallenge/InMemoryFeedStore.swift
@@ -0,0 +1,23 @@
+//
+//  InMemoryFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Ali Adam on 01/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+final class InMemoryFeedStore: FeedStore {
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		
+	}
+	
+	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		
+	}
+	
+	func retrieve(completion: @escaping RetrievalCompletion) {
+		
+	}
+}
